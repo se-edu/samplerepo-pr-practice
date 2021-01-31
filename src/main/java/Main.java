880163c3358219edecf4085445edac29165496d3
KimIdeas8 <Kberleytay@gmail.com>
2021-02-01 00:57:20 +0800

    1st commit :))

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+        System.out.println("Hiiii, do share your name :))");
+        //taking in user's name:
+        Scanner in = new Scanner(System.in);
+        String name = in.nextLine();
+        System.out.println("My name is " + name);
+    }
+
+}
