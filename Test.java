b2abcbf547d06f0f02091bfc44fe033c8a2baff2
YoengKokLeong <e0968894@u.nus.edu>
2024-03-05 22:22:11 +0800

    New branch

---
+++ Test.java
@@ -0,0 +1,12 @@
+public class Test {
+    String line;
+
+    public Test (){
+        this.line = "This is a test";
+    }
+
+    public static void main (String[] args) {
+        Test one = new Test();
+        System.out.println(one.line);
+    }
+}
